31ecf9dc67f52af4788273b6841d4c3f327e621e
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-04-12 19:27:11 -0300

    created: Declaring and implementing the Som class

---
+++ Command/Som.h
@@ -0,0 +1,16 @@
+#ifndef DISPOSITIVOSOM_H
+#define DISPOSITIVOSOM_H
+
+#include <iostream>
+#include <string>
+
+//Dispositivo Som;
+class Som
+{
+	public:
+		void ligar(std::string data);
+	
+		void desligar();
+};
+
+#endif
